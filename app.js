66b60d1166c4f14f4235de5fc61831c16afdc862
Aziza <elzairbekovaaa@gmail.com>
2024-10-10 12:27:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/*echo "# lesson1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/aziza417/lesson1.git
+git push -u origin main
+/*
